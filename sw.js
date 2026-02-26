a6036b3b4cc487be701adfbab2510fe750b7cad6
Gean Dioses <40503619+antoleod@users.noreply.github.com>
2026-02-26 14:08:58 +0100

    v1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE = "barra-scanner-v1";
-const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+﻿const CACHE = "barra-scanner-v2";
+const CORE = ["./", "./index.html", "./styles.css", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
@@ -15,11 +15,17 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
     caches.match(event.request).then((cached) =>
-      cached || fetch(event.request).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-        return res;
-      }).catch(() => caches.match("./index.html"))
+      cached ||
+      fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return res;
+        })
+        .catch(() => {
+          if (event.request.mode === "navigate") return caches.match("./index.html");
+          return undefined;
+        })
     )
   );
 });
