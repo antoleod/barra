1aace3fcafd4727da4bd13ea58ff3f25cdb95eca
Gean Dioses <40503619+antoleod@users.noreply.github.com>
2026-03-03 12:36:24 +0100

    SECURITY: Remove exposed firebase config file

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAovSN1HlHGw4hM_8ZABN-PTvlypzIPCPY",
-    authDomain: "lectorqr-45291.firebaseapp.com",
-    projectId: "lectorqr-45291",
-    storageBucket: "lectorqr-45291.firebasestorage.app",
-    messagingSenderId: "751229393866",
-    appId: "1:751229393866:web:1cce5fa16380435ca94d33",
-    measurementId: "G-BE9R1XH54E"
-};
